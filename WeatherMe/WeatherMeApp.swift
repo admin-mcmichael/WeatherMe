b8832f138e416e9c2b2e3ff6ca2db701965860d7
Luke <luke@mcmichael.com.au>
2025-07-26 10:29:03 +1000

    Initial Commit

---
+++ WeatherMe/WeatherMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMeApp.swift
+//  WeatherMe
+//
+//  Created by Luke on 26/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
